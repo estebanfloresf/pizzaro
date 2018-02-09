447ba90417702a655116f8c6d36f2c686ef37e82
Esteban <esteban_fs@hotmail.com>
2018-02-08 22:16:09 -0500

    Banner Images

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,58 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function () {
- return gulp.src('app/scss/**/*.scss')
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('app/css'))
-   .pipe(browserSync.reload({
-    stream: true
-  }));
-});
-
-gulp.task( 'default', [ 'sass:watch' ] )
+// gulp.task('sass', function () {
+//  return gulp.src('app/scss/**/*.scss')
+//    .pipe(sass().on('error', sass.logError))
+//    .pipe(gulp.dest('app/css'))
+//    .pipe(browserSync.reload({
+//     stream: true
+//   }));
+// });
 
 
-gulp.task('watch', ['browserSync','sass'], function () {
- gulp.watch('app/scss/**/*.scss', ['sass']);
- gulp.watch('app/*.html', browserSync.reload); 
- gulp.watch('app/js/**/*.js', browserSync.reload); 
-});
+// gulp.task('sass:watch', function(){
+//   gulp.watch('app/scss/**/*.scss', ['sass']);
+// });
+
+// gulp.task('watch', ['browserSync','sass'], function () {
+ 
+//  gulp.watch('app/*.html', browserSync.reload); 
+//  gulp.watch('app/js/**/*.js', browserSync.reload); 
+// });
 
 
-gulp.task('browserSync', function() {
+// gulp.task('browserSync', function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: './app'
+//     },
+//   });
+// });
+
+// gulp.task( 'default', [ 'sass:watch' ] );
+
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
   browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-})
+      server: "./app"
+  });
+
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch('app/js/*.js').on('change', browserSync.reload); 
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("app/scss/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("app/css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
