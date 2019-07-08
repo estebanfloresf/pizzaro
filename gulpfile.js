92c4b42d65cb8430240381a48326bb277f197c32
Esteban <esteban_fs@hotmail.com>
2019-07-07 20:47:08 -0500

    Changes
    Changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,34 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
-// gulp.task('sass', function () {
-//  return gulp.src('app/scss/**/*.scss')
-//    .pipe(sass().on('error', sass.logError))
-//    .pipe(gulp.dest('app/css'))
-//    .pipe(browserSync.reload({
-//     stream: true
-//   }));
-// });
-
-
-// gulp.task('sass:watch', function(){
-//   gulp.watch('app/scss/**/*.scss', ['sass']);
-// });
-
-// gulp.task('watch', ['browserSync','sass'], function () {
- 
-//  gulp.watch('app/*.html', browserSync.reload); 
-//  gulp.watch('app/js/**/*.js', browserSync.reload); 
-// });
-
-
-// gulp.task('browserSync', function() {
-//   browserSync.init({
-//     server: {
-//       baseDir: './app'
-//     },
-//   });
-// });
-
-// gulp.task( 'default', [ 'sass:watch' ] );
-
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-  browserSync.init({
-      server: "./app"
-  });
-
-  gulp.watch("app/scss/*.scss", ['sass']);
-  gulp.watch('app/js/*.js').on('change', browserSync.reload); 
-  gulp.watch("app/*.html").on('change', browserSync.reload);
-});
-
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("app/scss/*.scss")
-      .pipe(sass())
+// compile scss into css
+function style() {
+  // 1. Where is my scss file
+  return (
+    gulp
+      .src("app/scss/**/*.scss")
+      // 2. Pass that scss to the compiler
+      .pipe(sass().on("error", sass.logError))
+      // 3. where do we save the compiled CSS?
       .pipe(gulp.dest("app/css"))
-      .pipe(browserSync.stream());
-});
+      // 4. Stream changes to all browsers
+      .pipe(browserSync.stream())
+  );
+}
 
-gulp.task('default', ['serve']);
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./app"
+    }
+  });
+  gulp.watch("app/scss/**/*.scss", style);
+  gulp.watch("app/js/**/*.js").on("change", browserSync.reload);
+  gulp.watch("app/*.html").on("change", browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
